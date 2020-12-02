8a0347e7585050247787eea5c0df2084b9fcfc1e
apangasa <60531810+apangasa@users.noreply.github.com>
2020-12-02 16:48:03 -0500

    Maps random sample of audio to common names

---
+++ backend/renameRandomSample.py
@@ -0,0 +1,29 @@
+import os
+from shutil import copyfile
+import csv
+
+OLD_DIR = r'../data/Random Sample'
+NEW_DIR = r'../data/Random Sample Named'
+MAP = r'../data/0_10000MLFiles.csv'
+
+
+def rename():
+    for filename in os.listdir(OLD_DIR):
+        print(filename)
+        ml_num = filename[0:filename.index('.')]
+        ext = filename[filename.index('.'):]
+        with open(MAP, 'r') as legend:
+            # ln = 0
+            reader = csv.reader(legend, delimiter=',')
+            for row in reader:
+                if str(row[0]) == str(ml_num):
+                    new_name = str(row[3]) + ext
+                    if '/' in new_name:
+                        new_name = new_name.replace('/', ' ', 3)
+                    src = OLD_DIR + '/' + filename
+                    dest = NEW_DIR + '/' + new_name
+                    copyfile(src, dest)
+                    break
+
+
+rename()
