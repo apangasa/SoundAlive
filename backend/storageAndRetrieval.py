664e91e94492dc69595ff532202abdef53fefc4d
apangasa <60531810+apangasa@users.noreply.github.com>
2020-12-03 18:10:03 -0500

    Serialize and deserialize trees using pickle

--- backend/storageAndRetrieval.py
+++ backend/storageAndRetrieval.py
@@ -1,7 +1,16 @@
-def serialize(tree):
-    pass
+import pickle
+
+
+def serialize(tree, name):
+    with open('../data/created_data/' + name + '.pickle', 'wb') as tree_file:
+        pickle.dump(tree, tree_file)
 
 
 def deserialize():
-    pass
-    return (1, 2)
+    b_tree = None
+    s_tree = None
+    with open('../data/created_data/tree.pickle', 'rb') as tree_file:
+        b_tree = pickle.load(tree_file)
+    with open('../data/created_data/s.pickle', 'rb') as tree_file:
+        s_tree = pickle.load(tree_file)
+    return (b_tree, s_tree)
