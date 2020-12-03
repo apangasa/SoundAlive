baa27c88a9ccb855da195d19a8f89df8909e214d
apangasa <60531810+apangasa@users.noreply.github.com>
2020-12-03 18:03:30 -0500

    Test serialize/deserialize with pickle - it works!

--- backend/testBTree.py
+++ backend/testBTree.py
@@ -1,12 +1,25 @@
 from bTree import BTree
+import pickle
 
 
 tree = BTree(4)
 
-for i in range(100000):
-    tree.insert(pow(i, 4) % 567)
+x = [0, 1, 2, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3]
+
+for i in range(20):
+    tree.insert(i % 4)
 
 tree.print_order()
 
 print(tree.search(1))
 print(tree.search(2))
+
+with open('../data/created_data/tree.pickle', 'wb') as tree_file:
+    pickle.dump(tree, tree_file)
+
+tree = None
+
+
+with open('../data/created_data/tree.pickle', 'rb') as tree_file:
+    x = pickle.load(tree_file)
+    x.print_order()
