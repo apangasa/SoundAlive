63480f0cfad3bed3d4e9abc11be01800b3f8ac48
PraveenAnbu <52179198+PraveenAnbu@users.noreply.github.com>
2020-12-03 08:47:14 -0500

    Update signalProcessing.py

--- backend/signalProcessing.py
+++ backend/signalProcessing.py
@@ -1,3 +1,24 @@
-def process_signal(audio):
-    pass
-    return 0
+import numpy as np
+from scipy.io.wavfile import read
+import math
+
+
+def process_signal(animal):
+    fs, data = read(animal + ".wav")
+    data = data[:, 0]
+    data = data[(len(data)//2):]
+    w = np.arange(-1 * math.pi, math.pi + (math.pi/10), math.pi/10)
+    h = np.zeros(len(w), dtype=complex)
+    for x in range(len(w)):
+        for k in range(len(data)):
+            h[x] = h[x] + (data[k] * (math.cos(-1 * w[x] * k) + 1j*math.sin(-1 * w[x] * k)))
+    avg_data = np.average(data)
+    amp = max(data) - avg_data
+    h = np.abs(h)
+    avg_h = np.average(h)
+    for i in range(len(h)):
+        if h[i] > avg_h:
+            index = i
+    freq = abs(w[index])
+    output = amp * freq
+    return output
