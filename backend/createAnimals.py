d151142bd923dcc8500b51fd40b1f646b8aaa25a
apangasa <60531810+apangasa@users.noreply.github.com>
2020-12-03 18:03:09 -0500

    Update filepaths, fix len bug, fix typecast bug

--- backend/createAnimals.py
+++ backend/createAnimals.py
@@ -1,19 +1,24 @@
 import random
 import json
 
+COPIES = 14
+
 one_copy_dict = {}
-with open('our_key_2.json', 'r') as our_key:
+with open('../data/created_data/our_key_2.json', 'r') as our_key:
     one_copy_dict = json.load(our_key)
+
+print(len(one_copy_dict))
 
 multi_copy_dict = {}
 for item in one_copy_dict.items():
     animal = item[1]
     multi_copy_dict[item[0]] = animal
-    for i in range(len(14)):
+    for i in range(COPIES):
         x = random.uniform(-18.0, 18.0)
-        new_val = item[0] + x
-        multi_copy_dict[new_val] = animal
+        new_val = float(item[0]) + x
+        multi_copy_dict[str(new_val)] = animal
 
+print(len(multi_copy_dict))
 
-with open('our_key_3.json', 'w') as our_key:
+with open('../data/created_data/our_key_3.json', 'w') as our_key:
     json.dump(multi_copy_dict, our_key)
