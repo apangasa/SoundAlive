5032aa75e637e87641380ecb6be5fdd539c3a0c7
shlkllpr <sohil.kollipara@gmail.com>
2020-12-07 18:00:10 -0500

    Integrate Python Anywhere

--- soundalive/src/File.js
+++ soundalive/src/File.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import FileBase64 from 'react-file-base64';
 
+
+
 export default class File extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      files: []
+      files: [],
+      loading: false,
     }
   }
-
+  extractResult(result) {
+    let set = new Set();
+    result.Splay.matches.forEach(element =>
+      set.add(element)
+    );
+    result.B.matches.forEach(element =>
+      set.add(element)
+    );
+    set.delete(result.Splay.matches[0]);
+    this.props.onFileSend(result.Splay.matches[0], result.B.time, result.Splay.time, set);
+  }
   // Callback~
   getFiles(files){
     this.setState({ files: files })
@@ -18,27 +31,35 @@ export default class File extends React.Component {
     myHeaders.append("Content-Type", "application/json");
 
     var raw = JSON.stringify({"content":files.base64});
-
-      var requestOptions = {
+    var requestOptions = {
           method: 'POST',
           headers: myHeaders,
           body: raw,
           redirect: 'follow'
         };
 
-        fetch("https://70b6fcf64826.ngrok.io/process-audio", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .then(this.props.onFileSend("animal", "time"))
+        fetch("https://apangasa.pythonanywhere.com/process-audio", requestOptions)
+        .then(response => response.json())
+        .then(result =>
+          this.extractResult(result)
+        )
         .catch(error => console.log('error', error));
   }
 
   render() {
-    return (
+    if (!this.state.loading){
+      return (
+
       <FileBase64
         multiple={ false }
         onDone={ this.getFiles.bind(this) } />
-    )
+
+    )}
+    else {
+      return (
+        <p> Loading </p>
+      )
+    }
   }
 
 }
