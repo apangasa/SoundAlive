89b72ed861ce68312be6dc8549f622472fb5ec42
shlkllpr <sohil.kollipara@gmail.com>
2020-12-07 19:25:48 -0500

    Explain process of generating animal copies

--- backend/createAnimals.py
+++ backend/createAnimals.py
@@ -2,7 +2,7 @@ import random
 import json
 
 MIN_COPIES_PER_ANIMAL = 50
-
+#creating copies of animals with slight variation in dataset
 one_copy_dict = {}
 with open('../data/created_data/combined_raw.json', 'r') as our_key:
     one_copy_dict = json.load(our_key)
@@ -12,12 +12,12 @@ print(len(one_copy_dict))
 
 copied = {''}
 copied.remove('')
-
+#for each item, if not copied, add respective amounts of animals (if not enough animals) or remove animals if too many animals
 multi_copy_dict = {}
 for item in one_copy_dict.items():
     animal = item[1]
     multi_copy_dict[item[0]] = animal
-
+    #if correct number of copied animals continue
     if animal in copied:
         continue
 
@@ -25,7 +25,7 @@ for item in one_copy_dict.items():
     # print(instances)
 
     copies = 0
-
+    #If not enough animals, generate copies of animals or destroy extra copies
     if instances > MIN_COPIES_PER_ANIMAL:
         copied.add(animal)
         print(animal + ' ' + str(instances))
@@ -34,6 +34,7 @@ for item in one_copy_dict.items():
         copies = MIN_COPIES_PER_ANIMAL - instances
 
     for i in range(copies):
+        #randomizing copies with slight variation
         x = random.uniform(-18.0, 18.0)
         new_val = float(item[0]) + x
         multi_copy_dict[str(new_val)] = animal
@@ -43,6 +44,7 @@ for item in one_copy_dict.items():
 print('Full dataset size')
 print(len(multi_copy_dict))
 
+#Writing new dataset to file
 with open('../data/created_data/full_dataset.json', 'w') as our_key:
     json.dump(multi_copy_dict, our_key)
 
